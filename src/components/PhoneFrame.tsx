983203c46fd0556af0a57968ceb0b5c531f8348a
Hatem Soliman <hatemthedev@gmail.com>
2024-06-26 23:00:24 +0300

    Cleaned code

--- src/components/PhoneFrame.tsx
+++ src/components/PhoneFrame.tsx
@@ -71,7 +71,7 @@ export default function PhoneFrame({ screenshotList, color, buttonColor }: Phone
       onTouchEnd={handleTouchEnd}
     >
       {/* <img src={PhoneFrameset} alt="Image by svstudioart on Freepik" className="phone__frameset" /> */}
-      <div className={`w-[250.38px] h-[507.5px] border-[3px] ${color ? `border-[${color}]` : "border-gray-600"} rounded-[2.4rem] z-10 relative`}>
+      <div className={`w-[250.38px] h-[507.5px] border-[3px] border-gray-600 rounded-[2.4rem] z-10 relative`}>
         <div className="w-full h-full border-[9px] border-black p-2 rounded-[2.2rem]">
           <div className="bg-transparent rounded-lg flex justify-center items-start">
             <div className="w-1/3 h-4/12 bg-black rounded-full p-[0.4rem]">
@@ -95,6 +95,20 @@ export default function PhoneFrame({ screenshotList, color, buttonColor }: Phone
       />
       <div className="preview__scroll--btns">
         <button 
+          className={`preview__scroll--btn btn bg-[#004F98]`}
+          onClick={showPreviousImage}
+        >
+          Previous
+        </button>
+        <button 
+          className={`preview__scroll--btn btn bg-[#004F98]`}
+          onClick={showNextImage}
+        >
+          Next
+        </button>
+      </div>
+      {/* <div className="preview__scroll--btns">
+        <button 
           className={`preview__scroll--btn btn ${buttonColor ? `bg-[${buttonColor}]` : 'bg-[#004F98]'}`}
           onClick={showPreviousImage}
         >
@@ -106,7 +120,7 @@ export default function PhoneFrame({ screenshotList, color, buttonColor }: Phone
         >
           Next
         </button>
-      </div>
+      </div> */}
     </figure>
   );
 }
