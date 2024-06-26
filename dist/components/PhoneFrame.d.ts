0882c3c37c19e813b1a809ff7719b80250270f4a
Hatem Soliman <hatemthedev@gmail.com>
2024-06-26 18:38:51 +0300

    Added color customizability

--- dist/components/PhoneFrame.d.ts
+++ dist/components/PhoneFrame.d.ts
@@ -2,6 +2,7 @@ import React from 'react';
 import '../styles/styles.css';
 interface PhoneFrameProps {
     screenshotList: string[];
+    color?: string;
 }
-export default function PhoneFrame({ screenshotList }: PhoneFrameProps): React.JSX.Element;
+export default function PhoneFrame({ screenshotList, color }: PhoneFrameProps): React.JSX.Element;
 export {};
