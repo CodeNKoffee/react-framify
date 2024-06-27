9a4e5a3328c7cc135bc36554f7cbc9f683eb84ac
Hatem Soliman <hatemthedev@gmail.com>
2024-06-27 13:42:36 +0300

    Reconfigured Tailwind and removed unused dir

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: [
     './src/**/*.{js,jsx,ts,tsx}', // Adjust the paths according to your project structure
     './public/index.html',
+    './node_modules/react-framify/dist/**/*.js',
   ],
   theme: {
     extend: {},
