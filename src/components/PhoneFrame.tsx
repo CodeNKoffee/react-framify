d541c5ef78d40b1724e6e2d4f6f2dc68444831d7
Hatem Soliman <hatemthedev@gmail.com>
2024-06-27 14:22:47 +0300

    Replaced Tailwindcss with inline styling

--- src/components/PhoneFrame.tsx
+++ src/components/PhoneFrame.tsx
@@ -112,25 +112,69 @@ export default function PhoneFrame({ screenshotList, color, buttonColor }: Phone
           Next
         </button>
       </div> */}
-      <div className={`w-[250.38px] h-[507.5px] border-[3px] border-gray-600 rounded-[2.4rem] z-10 relative`}>
-        <div className="w-full h-full border-[9px] border-black rounded-[2.2rem] z-20">
+      <div
+        style={{
+          width: '250.38px',
+          height: '507.5px',
+          border: '3px solid #4A5568', // Tailwind's gray-600 is #4A5568
+          borderRadius: '2.4rem',
+          zIndex: 10,
+          position: 'relative',
+        }}
+      >
+        <div
+          style={{
+            width: '100%',
+            height: '100%',
+            border: '9px solid #000000',
+            borderRadius: '2.2rem',
+            zIndex: 20,
+          }}
+        >
           <img
             src={screenshotList[currentImageIndex]}
-            className={`phone__frameset--img ${fadeOut ? 'fade-out' : ''} relative rounded-[1.5rem] h-full w-full z-0`}
+            className={`phone__frameset--img ${fadeOut ? 'fade-out' : ''}`}
+            style={{
+              position: 'relative',
+              borderRadius: '1.5rem',
+              height: '100%',
+              width: '100%',
+              zIndex: 0,
+            }}
           />
-          <div className="w-1/3 h-4/12 bg-black rounded-full p-[0.4rem] absolute top-4 left-1/2 -translate-x-1/2">
-            <div className="float-right w-1/5 rounded-full bg-gray-900 p-[0.25rem]"></div>
+          <div
+            style={{
+              width: '33.33%', // 1/3 of the width
+              height: '33.33%', // 4/12 of the height
+              backgroundColor: '#000000',
+              borderRadius: '9999px', // full rounded
+              padding: '0.4rem',
+              position: 'absolute',
+              top: '1rem', // Tailwind's top-4
+              left: '50%',
+              transform: 'translateX(-50%)',
+            }}
+          >
+            <div
+              style={{
+                float: 'right',
+                width: '20%', // 1/5 of the width
+                borderRadius: '9999px', // full rounded
+                backgroundColor: '#1A202C', // Tailwind's gray-900
+                padding: '0.25rem',
+              }}
+            ></div>
           </div>
         </div>
       </div>
       <div className="preview__scroll--btns">
-        <button 
+        <button
           className={`preview__scroll--btn btn`}
           onClick={showPreviousImage}
         >
           Previous
         </button>
-        <button 
+        <button
           className={`preview__scroll--btn btn`}
           onClick={showNextImage}
         >
