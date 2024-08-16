205a3b2101a3bf016423c635f03487eef0c442b4
Hatem Soliman <hatemthedev@gmail.com>
2024-08-16 23:03:12 +0300

    Corrected IOS battery

--- src/components/PhoneFrame.tsx
+++ src/components/PhoneFrame.tsx
@@ -194,8 +194,8 @@ export default function PhoneFrame({
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 25 12"
-              width="25"
-              height="12"
+              width="16.5"
+              height="8"
               fill="none"
               stroke={indicatorColor}
               strokeWidth="1"
