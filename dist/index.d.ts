55389690989c72dbe46600a7f7f1f2911b5c00bf
Hatem Soliman <hatemthedev@gmail.com>
2024-08-17 20:33:18 +0300

    Fixed component export

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,9 @@
 import iPhoneFrame from "./components/iPhoneFrame";
 import AndroidFrame from "./components/AndroidFrame";
 import iPadFrame from "./components/iPadFrame";
-export { iPhoneFrame, AndroidFrame, iPadFrame };
+declare const _default: {
+    iPhoneFrame: typeof iPhoneFrame;
+    AndroidFrame: typeof AndroidFrame;
+    iPadFrame: typeof iPadFrame;
+};
+export default _default;
